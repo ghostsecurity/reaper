6d70c52e458cbd0d2d18f772f71168f58f4eadc3
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2023-01-12 18:08:53 -0500

    add prettier tailwind plugin

--- frontend/src/components/SettingsModal.vue
+++ frontend/src/components/SettingsModal.vue
@@ -42,7 +42,7 @@ function saveSettings(settings: Settings) {
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 mt-10">
+        <div class="mt-10 min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <TransitionChild
             as="template"
             enter="ease-out duration-300"
