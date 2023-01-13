d27aae61c6a89c4ee111c932dc3d3a866d442587
Liam Galvin <liam.galvin@ghost.security>
2023-01-13 17:01:30 +0000

    fix broken editor popout

--- frontend/src/components/Http/IDE.vue
+++ frontend/src/components/Http/IDE.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { XMarkIcon } from '@heroicons/vue/20/solid'
 import { PropType, ref, watch } from 'vue'
 import { HttpRequest } from '../../lib/Http'
 import Request from './HttpRequest.vue'
@@ -7,6 +8,8 @@ import Response from './HttpResponse.vue'
 const props = defineProps({
   request: { type: Object as PropType<HttpRequest>, required: true },
 })
+
+const emit = defineEmits(['close'])
 
 const currentTab = ref('request')
 
@@ -24,23 +27,28 @@ function selectTab(tab: string) {
 <template>
   <div style="height: calc(100% - 4rem)">
     <div>
-      <div class="border-b border:snow-storm-3 dark:border-polar-night-4">
-        <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-          <a @click="selectTab('request')" :class="['request' == currentTab ?
-                       'border-frost text-frost' :
-                       'border-transparent text-polar-night-4 hover:text-frost-4 hover:border-frost-4',
-          'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer']"
-            :aria-current="'request' == currentTab ? 'page' : undefined">
-            Request
-          </a>
-          <a v-if="request.Response" @click="selectTab('response')" :class="['response' == currentTab ?
-          'border-frost text-frost' :
-          'border-transparent text-polar-night-4 hover:text-frost-4 hover:border-frost-4',
-          'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer']"
-            :aria-current="'response' == currentTab ? 'page' : undefined">
-            Response
-          </a>
-        </nav>
+      <div class="border-b border:snow-storm-3 dark:border-polar-night-4 flex">
+        <div class="flex-1">
+          <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+            <a @click="selectTab('request')" :class="['request' == currentTab ?
+                         'border-frost text-frost' :
+                         'border-transparent text-polar-night-4 hover:text-frost-4 hover:border-frost-4',
+            'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer']"
+              :aria-current="'request' == currentTab ? 'page' : undefined">
+              Request
+            </a>
+            <a v-if="request.Response" @click="selectTab('response')" :class="['response' == currentTab ?
+            'border-frost text-frost' :
+            'border-transparent text-polar-night-4 hover:text-frost-4 hover:border-frost-4',
+            'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer']"
+              :aria-current="'response' == currentTab ? 'page' : undefined">
+              Response
+            </a>
+          </nav>
+        </div>
+        <div class="flex-0 cursor-pointer pt-3 text-gray-400 hover:text-snow-storm-1" @click="emit('close')">
+          <XMarkIcon class="w-6 h-6" />
+        </div>
       </div>
     </div>
     <div class="pt-4 h-full">
