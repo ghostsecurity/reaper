7cac4da45f66499c2a299fc607fd6253531cfe6c
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2023-01-14 19:46:06 -0500

    add secret Nord bg colors

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    variants: {
-        extend: {
-            textColor: ['hover', 'group-hover'],
-        }
+  variants: {
+    extend: {
+      textColor: ['hover', 'group-hover'],
     },
-    darkMode: 'class',
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx,vue}",
-    ],
-    theme: {
-        extend: {
-            /*
-      Nord
+  },
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      /*
 
+      These aren't part of the Nord palette, but they are the bg colors
+      used in the Nord website.
+      #f2f4f8 - light bg
+      #242933 - dark bg
+      
+      Nord - published palette
+      
       Polar Night: Backgrounds (dark to light
       #2e3440
       #3b4252
@@ -40,44 +43,45 @@ module.exports = {
       #b48ead pink
 
       */
-            colors: {
-
-                'polar-night': {
-                    DEFAULT: "#2e3440",
-                    '1': "#2e3440",
-                    '1a': "#353b47",
-                    '2': "#3b4252",
-                    '3': "#434c5e",
-                    '4': "#4c566a",
-                },
-
-                'snow-storm': {
-                    DEFAULT: '#eceff4',
-                    '1': '#d8dee9',
-                    '2': '#e5e9f0',
-                    '3': '#eceff4',
-                },
-
-                'frost': {
-                    DEFAULT: "#8fbcbb",
-                    '1': "#8fbcbb",
-                    '2': "#88c0d0",
-                    '3': "#81a1c1",
-                    '4': "#5e81ac",
-                },
-
-                'aurora': {
-                    DEFAULT: "#bf616a",
-                    '1': "#bf616a",
-                    '2': "#d08770",
-                    '3': "#ebcb8b",
-                    '4': "#a3be8c",
-                    '5': "#b48ead",
-                },
-            },
+      colors: {
+        reaper: {
+          'bg-dark': '#242933',
+          'bg-light': '#f2f4f8',
         },
+        'polar-night': {
+          DEFAULT: '#2e3440',
+          1: '#2e3440',
+          '1a': '#353b47',
+          2: '#3b4252',
+          3: '#434c5e',
+          4: '#4c566a',
+        },
+
+        'snow-storm': {
+          DEFAULT: '#eceff4',
+          1: '#d8dee9',
+          2: '#e5e9f0',
+          3: '#eceff4',
+        },
+
+        frost: {
+          DEFAULT: '#8fbcbb',
+          1: '#8fbcbb',
+          2: '#88c0d0',
+          3: '#81a1c1',
+          4: '#5e81ac',
+        },
+
+        aurora: {
+          DEFAULT: '#bf616a',
+          1: '#bf616a',
+          2: '#d08770',
+          3: '#ebcb8b',
+          4: '#a3be8c',
+          5: '#b48ead',
+        },
+      },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
+  },
+  plugins: [require('@tailwindcss/forms')],
 }
