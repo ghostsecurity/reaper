871684ba9f855206f659be0ac3564fbc5f23f40f
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2023-01-14 19:46:31 -0500

    add shared Button components

---
+++ frontend/src/components/Shared/ButtonCancel.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    type="button"
+    class="ml-2 inline-flex items-center rounded border border-transparent bg-aurora-1 px-2.5 py-1.5 text-xs font-semibold text-polar-night-1 shadow-sm hover:bg-aurora-1/80 focus:outline-none">
+    <slot></slot>
+  </button>
+</template>
