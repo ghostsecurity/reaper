3b82ea58fa160409833ecad332a1a5d9be4bed60
Liam Galvin <liam.galvin@ghost.security>
2023-04-22 10:02:40 +0100

    Fix validation

--- backend/workflow/workflow.go
+++ backend/workflow/workflow.go
@@ -26,6 +26,33 @@ func (w *Workflow) Run(ctx context.Context, updateChan chan<- Update) error {
 	return newRunner(ctx, w).Run(updateChan)
 }
 
+func (w *Workflow) Validate() error {
+	for _, node := range w.Nodes {
+		if err := w.validateNode(node); err != nil {
+			return err
+		}
+	}
+	if err := w.validateNode(w.Output); err != nil {
+		return err
+	}
+	if err := w.validateNode(w.Error); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (w *Workflow) validateNode(node Node) error {
+	for _, input := range node.GetOutputs() {
+		if err := input.Type.validate(); err != nil {
+			return err
+		}
+	}
+	if err := node.GetInput().Type.validate(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (w *Workflow) FindNode(id uuid.UUID) (Node, error) {
 	for _, n := range w.Nodes {
 		if n.ID() == id {
