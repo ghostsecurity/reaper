89b18177f8324e9e52f029ceb515d579613ee165
Liam Galvin <liam.galvin@ghost.security>
2023-08-17 11:41:14 +0100

    Align workspace button (#82)
    * Align workspace button

--- frontend/src/components/WorkspaceMenu.vue
+++ frontend/src/components/WorkspaceMenu.vue
@@ -15,30 +15,30 @@ const emit = defineEmits(['switch', 'edit'])
   <Menu as="div" class="relative inline-block text-left">
     <div>
       <MenuButton
-        class="overflow-y-clip max-h-10 inline-flex w-full justify-center rounded-md border border-frost-2 bg-frost-2 px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:ring-2 hover:ring-snow-storm-1">
+          class="overflow-y-clip inline-flex w-full justify-center rounded-md border border-frost-2 bg-frost-2 px-4 py-2 text-m font-medium text-gray-700 shadow-sm hover:ring-2 hover:ring-snow-storm-1">
         <i>{{ props.ws.name }}</i>
-        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true"/>
       </MenuButton>
     </div>
 
     <transition
-      enter-active-class="transition ease-out duration-100"
-      enter-from-class="transform opacity-0 scale-95"
-      enter-to-class="transform opacity-100 scale-100"
-      leave-active-class="transition ease-in duration-75"
-      leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95">
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95">
       <MenuItems
-        class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div class="py-1">
           <MenuItem v-slot="{ active }">
             <a
-              @click="emit('edit')"
-              :class="[
+                @click="emit('edit')"
+                :class="[
                 active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                 'group flex items-center px-4 py-2 text-sm cursor-pointer',
               ]">
-              <Cog6ToothIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+              <Cog6ToothIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true"/>
               Settings
             </a>
           </MenuItem>
@@ -46,12 +46,12 @@ const emit = defineEmits(['switch', 'edit'])
         <div class="py-1">
           <MenuItem v-slot="{ active }">
             <a
-              @click="emit('switch')"
-              :class="[
+                @click="emit('switch')"
+                :class="[
                 active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                 'group flex items-center px-4 py-2 text-sm cursor-pointer',
               ]">
-              <ArrowsRightLeftIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+              <ArrowsRightLeftIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true"/>
               Switch workspace...
             </a>
           </MenuItem>
