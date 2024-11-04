13b62206314d686390771c5fde8a07f059172365
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2024-11-04 17:32:32 -0500

    debug

--- tests/docker-ui.spec.ts
+++ tests/docker-ui.spec.ts
@@ -6,6 +6,6 @@ test('sign in button', async ({ page }) => {
   // Click the sign in button.
   await page.getByRole('button', { name: 'Sign in' }).click();
 
-  // Expects page to have a heading with the welcome message.
-  await expect(page.getByRole('heading', { name: 'Welcome to Reaper' })).toBeVisible();
+  // Expects page to have a heading with Scan title.
+  await expect(page.getByRole('heading', { name: 'Scan' })).toBeVisible();
 });
