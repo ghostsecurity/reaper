f00c225d3d4f93a7115f4c76d40397625d834c2e
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2024-11-04 17:05:55 -0500

    npm

--- tests/docker-ui.spec.ts
+++ tests/docker-ui.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('sign in button', async ({ page }) => {
   await page.goto('http://localhost:8000');
 
-  // Click the get started link.
+  // Click the sign in button.
   await page.getByRole('button', { name: 'Sign in' }).click();
 
-  // Expects page to have a heading with the name of Installation.
+  // Expects page to have a heading with the welcome message.
   await expect(page.getByRole('heading', { name: 'Welcome to Reaper' })).toBeVisible();
 });
