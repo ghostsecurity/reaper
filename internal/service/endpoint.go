1b5b7f56cf3f8c37d248cd83f7e8ec3b227e2034
Josh Larsen <2565382+joshlarsen@users.noreply.github.com>
2024-11-05 10:42:20 -0500

    fix/save endpoint params (#140)
    * fix 'make dev'
    
    * cleanup ui test
    
    * dynamic POST input params
    
    * update ui build

--- internal/service/endpoint.go
+++ internal/service/endpoint.go
@@ -9,22 +9,29 @@ type EndpointInput struct {
 	Method   string `json:"method"`
 	Hostname string `json:"hostname"`
 	Path     string `json:"path"`
+	Params   string `json:"params"`
 }
 
-func CreateEndpoint(db *gorm.DB, ep EndpointInput) (models.Endpoint, error) {
+// CreateOrUpdateEndpoint creates a new endpoint or updates an existing one with params
+func CreateOrUpdateEndpoint(db *gorm.DB, ep EndpointInput) (models.Endpoint, error) {
 	endpoint := models.Endpoint{
 		Hostname: ep.Hostname,
 		Method:   ep.Method,
 		Path:     ep.Path,
+		Params:   ep.Params,
 	}
 
 	resp := db.Where(models.Endpoint{
 		Hostname: ep.Hostname,
 		Path:     ep.Path,
 		Method:   ep.Method,
-	}).FirstOrCreate(&endpoint)
-	if resp.Error != nil {
-		return models.Endpoint{}, resp.Error
+	}).First(&endpoint)
+	if resp.RowsAffected == 0 {
+		db.Create(&endpoint)
+	}
+	if resp.RowsAffected == 1 {
+		endpoint.Params = ep.Params
+		db.Save(&endpoint)
 	}
 
 	return endpoint, nil
